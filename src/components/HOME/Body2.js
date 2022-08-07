401cb4486cecb4898f8b0c33526c9a800de74723
manishbhurtel <manishbhurtel668@gmail.com>
2022-08-07 18:25:00 +0545

    sd

--- src/components/HOME/Body2.js
+++ src/components/HOME/Body2.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import '../HOME/CSS/Body2.css'
+import '../../.././src/components/All.css'
+import manLOGO from '../../.././src/images/man.png'
 
 function Body2(props) {
   return (
     <>
       <div className="body2_container">
         <div className="textarea">
-          <p>{props.para4}</p>
-          <p>{props.para5}</p>
-          <button>See More</button>
+          <p className='para4'>{props.para4}</p>
+          <p className='para5'>{props.para5}</p>
+          <button className = "b2-btn1">See More</button>
         </div>
+        <img src={manLOGO} alt="manImg" className='manImg'/>
       </div>
     </>
   )
